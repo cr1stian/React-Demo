6de58656507799aa4d71071a1c78836f9864e203
Cristian Herrera <cristianhmal@gmail.com>
2018-07-29 01:45:30 -0700

    Shopping Cart quantity Add

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,42 +2,19 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
-
   render() {
+    const { onReset, onDelete, onIncrement, counters } = this.props; //Using Object Destructuring to not repeat this.props when calling props
     return (
       <div>
-        <button
-          className="btn btn-primary btn-sm m-2"
-          onClick={this.handleReset}
-        >
+        <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
           Reset
         </button>
 
-        {this.state.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
+
+//this is a Stateless Functional Component
